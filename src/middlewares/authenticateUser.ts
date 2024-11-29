7af6aae70e2848045e5ac823b940f4cc4b89c3a3
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-29 18:05:09 +0100

    feat(auth): added authenticateUser

---
+++ src/middlewares/authenticateUser.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel";
+
+export const authenticateUser = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
