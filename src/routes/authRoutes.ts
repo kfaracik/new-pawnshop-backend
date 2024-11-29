618e0946b0d0c7b3c4099bfc34bebe8e24beb058
Krzysztof <larkfreeme70.55@gmail.com>
2024-11-29 18:05:25 +0100

    feat(auth): added authenticateUser middleware

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "../controllers/authController";
+import { authenticateUser } from "../middlewares/authenticateUser";
 
 const router = Router();
 
+router.get("/user", authenticateUser, authController.getUserData);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
